763c45a405fd03a496ed6397b3b8c848618d8f03
disoul <starky_ds@sina.cn>
2017-02-25 15:16:43 +0800

    :sparkles: get user login info

---
+++ app/script/content/index.js
@@ -0,0 +1,46 @@
+/*
+ * background.js
+ * Copyright (C) 2017 disoul <disoul@DiSouldeMacBook-Pro.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+$('docuemnt').ready(function() {
+  var forms = $('form');
+  console.log('forms', forms);
+  forms.each(function(i) {
+    var form = $(this);
+    console.log('currentform', form);
+    var inputs = form.find('input');
+
+    var currentForm = {
+      user: null,
+      password: null,
+    };
+    var lastInputNode = null;
+
+    console.log('inputs', inputs);
+    inputs.change(function() {
+      var input = $(this)[0];
+      console.log('change', input);
+
+      // 用户开始更换输入框
+      if (lastInputNode !== input) {
+        // 如果用户当前切换到的输入框是密码输入框
+        if (input.type === 'password') {
+          // 将用户上一次输入的内容认为是user字段存入
+          currentForm.user = lastInputNode;
+          currentForm.password = input;
+        }
+      } else if (input.type === 'password') {
+        currentForm.password = input;
+      }
+
+      lastInputNode = input;
+      console.log('currentForm', currentForm);
+    })
+
+    form.submit(function(e) {
+      console.log(this, currentForm);
+    });
+  });
+})
